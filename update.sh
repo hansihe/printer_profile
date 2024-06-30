1b31dcd24b7c7a247a5e61bae7a7f17dd438f6d7
Hans Elias Bukholm Josephsen <hans@bukholm.com>
2024-06-30 13:12:54 +0200

    add idx file

--- update.sh
+++ update.sh
@@ -2,3 +2,4 @@
 set -ex
 
 wget https://raw.githubusercontent.com/hansihe/printer_profile/master/Hans.ini -O ~/.config/PrusaSlicer/vendor/Hans.ini
+wget https://raw.githubusercontent.com/hansihe/printer_profile/master/Hans.idx -O ~/.config/PrusaSlicer/vendor/Hans.idx
